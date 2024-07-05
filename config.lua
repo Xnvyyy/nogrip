367341c80ee9ef73b4bb656a72aeb9461dab276e
Xnvyy <84394810+Xnvyyy@users.noreply.github.com>
2024-07-05 11:45:19 +0200

    Added english subtitles

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 cfg = {}
 
-cfg.Upadek = 0.9            -- [szansa na upadnięcie przy skakaniu | 0.9 - 90%]
-cfg.CzasUpadku = 0.6        -- [W SEKUNDACH | !!nie polecam edytować!! czas upadku ]
-cfg.CameraShake = 0.08      -- [jak bardzo ma trzęść się kamera podczas upadku]
-cfg.Cooldown = 2            -- [W SEKUNDACH | ile sekund skrypt ma czekać przed kolejnym wykonaniem]
+cfg.Upadek = 0.9            -- [PL] szansa na upadnięcie przy skakaniu | 0.9 - 90%]             [ENG} CHANCE OF FALLING WHILE JUMPING | 0.9 - 90%
+cfg.CzasUpadku = 0.6        -- [PL] [W SEKUNDACH] | !!nie polecam edytować!! czas upadku            [ENG] [IN SECONDS] !!It is better to not edit this!! falling time
+cfg.CameraShake = 0.08      -- [PL] jak bardzo ma trzęść się kamera podczas upadku          [ENG] Camera shake level
+cfg.Cooldown = 2            -- [PL} W SEKUNDACH | ile sekund skrypt ma czekać przed kolejnym wykonaniem     [ENG] [IN SECONDS] | How many second should it wait before checking for player jumping once more
 function DoNotify()
  print('Potknąłeś się!')
- -- twoje notify
+ -- twoje notify -- your notify
 end
