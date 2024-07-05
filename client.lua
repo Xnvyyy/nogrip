d565af472c837a32d17b7365bb9691019b95f9f2
Xnvyy <84394810+Xnvyyy@users.noreply.github.com>
2024-07-05 12:27:36 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,13 +1,13 @@
 local ragdoll = cfg.Upadek
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(100)
+		Citizen.Wait(500)  -- higher value --> better optimization \ wieksza wartosc --> lepsza optymalizacja
 		local ped = PlayerPedId()
 		if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
 			local wynik = math.random()
 			if wynik < ragdoll then 
 				Citizen.Wait(cfg.CzasUpadku * 1000)
-                ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', cfg.CameraShake)
+                		ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', cfg.CameraShake)
 				SetPedToRagdoll(ped, 5000, 1, 2)
 				DoNotify()
 			else
